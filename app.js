fcade44e836f68431c7619e585ba4c136fa3a680
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2025-10-13 09:20:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-function criaCartao(categoria, pergunta, resposta) {}
+function criaCartao(categoria, pergunta, resposta) {
+  let container = document.getElementById('container');
+  let cartao = document.createElement('article');
+  cartao.className = 'cartao';
+}
 
